7027f0f04f9f55554dd50110d2d95965c6e072c9
Anders Eriksen <anders.grimsrud.eriksen@gmail.com>
2012-07-09 14:25:36 +0300

    Update master

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name = "django-varnish",
-    version = '0.1',
+    version = '0.1.1',
     url = 'http://opensource.washingtontimes.com/projects/django-varnish/',
     author = 'Justin Quick',
     author_email= 'justquick@gmail.com',
